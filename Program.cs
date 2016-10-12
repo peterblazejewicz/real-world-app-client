589b604fbb6f58d99eaf69e789ebe0177c803262
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-10-12 22:29:14 +0200

    Dotnet client configuration

--- Program.cs
+++ Program.cs
@@ -1,22 +1,19 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
-namespace EmptyWebApplication
+namespace RealWorldApp.Client
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-           var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("hosting.json", optional: false)
+                 .AddCommandLine(args)
+                 .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                 .Build();
             var host = new WebHostBuilder()
                 .UseConfiguration(config)
                 .UseKestrel()
@@ -24,7 +21,6 @@ namespace EmptyWebApplication
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
-
             host.Run();
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -1,36 +1,42 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace EmptyWebApplication
+namespace RealWorldApp.Client
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfigurationRoot Configuration { get; }
+        public Startup(IHostingEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.MapWhen(context =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
+                var path = context.Request.Path.Value;
+                return (!path.Contains("."));
+            }, aBranch =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                aBranch.Use((context, next) =>
+                {
+                    context.Request.Path = new PathString("/index.html");
+                    return next();
+                });
+                aBranch.UseStaticFiles();
             });
         }
     }
