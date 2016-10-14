5c0cc32569547c8bd8f57cea5c1da729fc25cfb3
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-10-14 21:06:26 +0200

    Update information about *require* fix

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,4 +3,5 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
+// https://github.com/angular/angular-cli/issues/2221
 declare var require: any;
